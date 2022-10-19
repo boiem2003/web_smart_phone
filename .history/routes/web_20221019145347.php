4d9eccd9972f92d65b0c8b852f44d7818683f9f2
khoa147 <nguyendkhoa147@gmail.com>
2022-10-19 15:05:53 +0700

    reup code

---
+++ .history/routes/web_20221019145347.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Admin\CategoryController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth','isAdmin'])->group(function () {
+    Route::get('/dashboard', 'Admin\FrontendController@index');
+    // Category
+    Route::get('categories', 'Admin\CategoryController@index');
+    Route::get('add-category', 'Admin\CategoryController@add');
+    Route::get('insert-category', 'Admin\CategoryController@insert');
+    Route::get('edit-prod/{id}', [CategoryController::class , 'edit']);
+    Route::put('update-category/{id}', [CategoryController::class, 'update']);
+    Route::get('delete-category/{id}', [CategoryController::class , 'destroy']);
+});
