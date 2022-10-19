a83d7fa3dea33c0911042c85b2b8637113587b27
NCThanh009 <nguyencongthanh111213@gmail.com>
2022-10-19 15:20:57 +0700

    test

--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -5,9 +5,10 @@
             <h4>Add category</h4>
         </div>
         <div class="card-body">
-            <form action ="{{ url('insert-category') }}" method = "PUT" enctype="multipart/form-data">
+            <form action ="{{ url('insert-category') }}" method = "POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
+                {{ csrf_field() }}
                 <div class="row">
                     <div class="col-md-6">
                         <label for="">Name</label>
@@ -42,10 +43,10 @@
                         <textarea name="meta_description" rows="3" class="form-control"></textarea>
                     </div>
                     <div class="col-md-12">
-                    <input type="file" name="image" class="form-control">
+                        <input type="file" name="image" class="form-control">
                     </div>
                     <div class="col-md-12">
-                        <button type="submit " class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
                 </div>
             </form>
