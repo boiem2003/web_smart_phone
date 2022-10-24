3ed91d5dc464b97a26ffc5e328fa41b3079a3750
NCThanh009 <nguyencongthanh111213@gmail.com>
2022-10-24 13:58:02 +0700

    test

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -17,7 +17,7 @@ class FrontendController extends Controller
         return view('frontend.index', compact('featured_products', 'trending_category'));
     }
     public function category(){
-        $category= Category::where('status','0')->get();
+        $category= Category::all();
         return view('frontend.category', compact('category'));
     }
     public function viewcategory($slug){
